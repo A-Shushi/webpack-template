fd56910c204e6964edd616af4c4740efd0889806
A-Shushi <219606551+A-Shushi@users.noreply.github.com>
2026-02-24 10:34:46 +0700

    Add Prettier and Setup with ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,28 @@
 import js from "@eslint/js";
 import globals from "globals";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+    js.configs.recommended,
+    prettierConfig,
+    {
+        files: ["**/*.{js,mjs,cjs}"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+        plugins: {
+            prettier: prettierPlugin,
+        },
+        rules: {
+            "prettier/prettier": "error",
+            "no-unused-vars": "warn",
+        },
+    },
 ]);
